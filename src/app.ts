d0eda8a616e565016d4f934b195b38ffdaa5c32b
Tarcila Garcia <tarcilasg@gmail.com>
2022-09-25 22:57:09 -0300

    created cors

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,12 @@ import loginRoutes from "./routes/login.routes";
 import adminRoutes from "./routes/admin.routes";
 import contactRoutes from "./routes/contact.routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use("/users", userRoutes);
 app.use("/login", loginRoutes);
 app.use("/adm", adminRoutes);
